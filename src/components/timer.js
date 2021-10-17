5424f5d31c04f76ace6c5fa0b203eb48d29f01e2
Andres Ramirez <andres.ramirez@boatyardx.com>
2021-10-16 23:53:03 -0500

    Wim hof method form styles and timer functionality

---
+++ src/components/timer.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+
+export const useTimer = (initialTime = 0, active = false) => {
+  const [seconds, setSeconds] = useState(initialTime);
+  const [isActive, setIsActive] = useState(active);
+  const [time, setTime] = useState("00:00");
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+    setTime("00:00");
+  }
+
+  function format(seconds) {
+    let minutes = "";
+    let newSeconds = "";
+
+    if (seconds <= 9) {
+      return `00:0${seconds}`;
+    } else if (seconds > 9 && seconds <= 60) {
+      return `00:${seconds}`;
+    } else if (seconds > 60) {
+      newSeconds = formatDigits(seconds - Math.round(seconds / 60) * 60);
+      minutes = formatDigits(Math.round(seconds / 60));
+      return `${minutes}:${newSeconds}`;
+    }
+  }
+
+  function formatDigits(value) {
+    if (value <= 9) {
+      return `0${value}`;
+    } else {
+      return value;
+    }
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+        const newTime = format(seconds);
+        setTime(newTime);
+        console.log("TIMER", time);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  return [time, isActive, toggle, reset];
+};
--- src/components/wim.js
+++ src/components/wim.js
@@ -1,18 +1,33 @@
 import "./counter.css";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
+import { useTimer } from "../components/timer";
 
 function Wim({ title }) {
   const pointerElement = useRef(null);
-  const start = () => {
-    pointerElement.current.classList.add("grow");
-  };
+  const selectedBlocksElement = useRef(null);
+  let currentBlocks = 3;
+  const [time, isActive, toggle, reset] = useTimer(0, false);
+
+  useEffect(() => {
+    pointerElement.current.addEventListener("animationend", () => {
+      stop();
+
+      // Activate Timer
+      toggle();
+    });
+  }, []);
 
   const stop = () => {
     pointerElement.current.classList.remove("grow");
+    reset();
   };
 
-  const changeSets = (e) => {
-    console.log(e.target.value);
+  const start = () => {
+    pointerElement.current.classList.add("grow");
+    reset();
+  };
+
+  const changeBreaths = (e) => {
     document.documentElement.style.setProperty(
       "--wim-iteration",
       e.target.value
@@ -21,55 +36,58 @@ function Wim({ title }) {
   };
 
   const changeSpeed = (e) => {
-    console.log(e.target.value);
-    document.documentElement.style.setProperty(
-      "--wim-time",
-      e.target.value.toString() + "s"
-    );
-    stop();
-  };
-
-  const changeBlocks = (e) => {
-    console.log(e.target.value);
-    document.documentElement.style.setProperty("--wim-blocks", e.target.value);
+    const speedTime = e.target.checked ? 2 : 3;
+    document.documentElement.style.setProperty("--wim-time", speedTime + "s");
     stop();
   };
 
   return (
-    <div className="wimhof">
+    <div className="wimhof breath-wrapper">
       <h4 className="title">{title}</h4>
       <div ref={pointerElement} className="circle"></div>
-      <h4 className="instructions">Prepare</h4>
       <div className="options">
-        <div className="input-block">
-          <label htmlFor="sets">Sets</label>
-          <input
-            id="sets"
-            type="number"
-            name="sets"
-            onChange={changeSets}
-            placeholder="30 breaths"
-          ></input>
+        <div className="card yellow hold-time first">
+          <p>BreathHold Time</p>
+          <h2 className="hold-time">{time}</h2>
         </div>
-        <div className="input-block">
-          <label htmlFor="blocks">Blocks</label>
-          <input
-            id="blocks"
-            type="number"
-            name="blocks"
-            onChange={changeBlocks}
-            placeholder="3 Blocks"
-          ></input>
-        </div>
-        <div className="input-block">
-          <label htmlFor="blocks">Breathing Speed</label>
-          <input
-            id="speed"
-            type="number"
-            name="speed"
-            onChange={changeSpeed}
-            placeholder="3 seconds"
-          ></input>
+        <div className="card blue second">
+          <div className="input-block">
+            <label htmlFor="sets">Breaths</label>
+            <input
+              id="sets"
+              type="number"
+              name="sets"
+              onChange={changeBreaths}
+              placeholder="30 breaths"
+            ></input>
+          </div>
+          <div className="input-block">
+            <label htmlFor="speed">Breathing Speed</label>
+
+            <div className="speed-block">
+              <div class="switch-button">
+                <input
+                  class="switch-button-checkbox"
+                  onChange={changeSpeed}
+                  type="checkbox"
+                  data-value="2"
+                ></input>
+                <label class="switch-button-label" for="">
+                  <span class="switch-button-label-span">Normal</span>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className=" instructions">
+            <h4>Instructions</h4>
+            <p>
+              Put the number or breaths that you will do, after finish, the
+              counter will start, hold your breath after the last exhalation as
+              much as you can, once you can't continue, push stop breathHold
+              button, full inhale and maintain 15 seconds. Repeat many times as
+              you want. Is recommended to start with 3 blocks.
+            </p>
+          </div>
         </div>
       </div>
       <button className="button start" onClick={start}>
